c4b73da21f123eb33bd0ce8a0001b5217b3bca0b
Chris Akridge <cakridge2@gmail.com>
2018-01-10 12:59:43 -0500

    Files for Visual Studio and GitHub courses

---
+++ TreehouseCoursework/TreehouseCoursework/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseCoursework
+{
+	public static class CollectionExtensions
+	{
+		public static void RemoveAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+			Func<TValue, bool> predicate)
+		{
+			// Credit to aku http://stackoverflow.com/a/469262/2709212
+			var keys = dictionary.Keys.Where(k => predicate(dictionary[k])).ToList();
+			foreach (var key in keys)
+			{
+				dictionary.Remove(key);
+			}
+		}
+	}
+}
